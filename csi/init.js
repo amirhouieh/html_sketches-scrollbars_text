f16fc261f625ef0120a07df0d9c1948634a9988b
amir houieh <xtxt@amirs-iMac.local>
2018-11-04 14:05:00 +0100

    init from 2015

---
+++ csi/init.js
@@ -0,0 +1,14 @@
+$(function(doc) {
+
+	var c = new Controller();
+		c.init();
+
+	$(window).resize( c.updateSize );
+	// $(window).mousemove( c.updateScrollTopOnMove );
+});
+
+
+var content = '<h1>Thought Trends in Graphic Design </h1>';
+	content += '<br><h3>The Forces which change students authority in the field of graphic design.</h3>';
+	content +='<br>Amir Houieh<br>';
+	content +='<br><br>Academies as major environments to shape our thoughts. And KABK — specifically Graphic Design Department — as an institution of involvement inherent in the social power relation, tried to not only shape your thoughts but also criticise them. To uncover the relational and social preconditions of KABK, I therefore based my thesis on data from various text sources from another academy thesis of graduated year students. and from it self. The idea is to show the theme and pattern of chosen topics for the thesis within the last 5 years. And un-mapping this patterns within this authorisation.'
